e4dad1f0d37b4fe60a4300cfebf3061771952c29
Jashanpreet Kaur <jashan100106@gmail.com>
2025-08-06 18:45:59 +0530

    Added 3 full stack experiments

---
+++ experiment3/script.js
@@ -0,0 +1,15 @@
+const html = document.querySelector("html");
+const checkbox = document.querySelector("#toggle-theme");
+const theme = document.querySelector("#theme-name");
+
+function toggleTheme() {
+  html.classList.toggle("dark");
+
+  if (html.classList.contains("dark")) {
+    theme.textContent = "Light Mode";
+  } else {
+    theme.textContent = "Dark Mode";
+  }
+}
+
+checkbox.addEventListener("change", toggleTheme);
