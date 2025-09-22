59d6c3e6fc181e302100f41a5006f79d76302c91
Jashanpreet Kaur <jashan100106@gmail.com>
2025-09-22 20:47:19 +0530

    Experiment-7

---
+++ experiment-7/src/components/product-card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ProductCard(props) {
+  return (
+    <div className="product-card">
+      <h3>{props.name}</h3>
+      <p>Price: ₹{props.price}</p>
+      <p>Status: {props.inStock ? "In Stock" : "Out of Stock"}</p>
+    </div>
+  );
+}
+
+export default ProductCard;
